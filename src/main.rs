728ef9e40fd8b6f7fc67eac952ac37e5d156d756
huchanghao <2955568032@qq.com>
2023-06-30 16:51:31 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::{env, fs};
 
 fn main() {
-    let res = env::args().collect::<Vec<String>>();
-    let query = &res[1];
-    let filename = &res[2];
-    let res = fs::read_to_string(filename)
-    .expect("something wrong!!");
+    let args = env::args().collect::<Vec<String>>();
+    let config = Config::new(&args);
+    let res = fs::read_to_string(config.filename).expect("something wrong!!");
     println!("{res}")
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
